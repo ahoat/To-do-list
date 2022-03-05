58c92bf68f3357822d79633ac7198fef3ad1f405
Minju Kim <kmj673@gmail.com>
2022-03-05 21:11:51 +0000

    created checkbox in list
    Relates #3

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
-const input = document.querySelector('.userInput');
-const addBtn = document.querySelector('.add-btn');
-const ul = document.querySelector('.list');
+const input = document.querySelector(".userInput");
+const addBtn = document.querySelector(".add-btn");
+const ul = document.querySelector(".list");
 
 //function to create list element
-function createListItem () {
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-};
+function createListItem() {
+  let li = document.createElement("li");
+  let check = document.createElement("input");
+  check.setAttribute("type", "checkbox");
+  li.appendChild(check);
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
 
 // add list item when button is clicked
-addBtn.addEventListener('click', function () {
-    if (input.value.length > 0) {
-        createListItem();
-    }
+addBtn.addEventListener("click", function () {
+  if (input.value.length > 0) {
+    createListItem();
+  }
 });
 
 //add list item when enter is pressed in input box
-input.addEventListener('keypress', function (event) {
-    if (input.value.length > 0 && event.which === 13) {
-        createListItem();
-    }
+input.addEventListener("keypress", function (event) {
+  if (input.value.length > 0 && event.which === 13) {
+    createListItem();
+  }
 });
