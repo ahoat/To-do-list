fd401d8646c11c169fd6b0cc512f397892971140
AhoaT <ahoa.tran@gmail.com>
2022-03-08 00:02:21 +0000

    fixed listItem class

--- scripts.js
+++ scripts.js
@@ -2,11 +2,21 @@ const input = document.querySelector(".userInput");
 const addBtn = document.querySelector(".add-btn");
 const ul = document.querySelector(".list");
 
+let listArr = [];
+
+//function to save to do list in local storage
+function saveToDo() {
+  localStorage.setItem("to-do-list", JSON.stringify(listArr));
+}
+
 //function to create list element
 function createListItem() {
   let li = document.createElement("li");
+  li.classList.add("listItem");
+
   let check = document.createElement("input");
   check.setAttribute("type", "checkbox");
+
   let deleteItem = document.createElement("button");
   deleteItem.classList.add("delete");
   deleteItem.innerText = "Delete";
@@ -15,9 +25,13 @@ function createListItem() {
   li.appendChild(document.createTextNode(input.value));
   li.appendChild(deleteItem);
   ul.appendChild(li);
+  listArr.push(input.value);
+  saveToDo();
+  console.log(li);
   input.value = "";
 };
 
+//function to remove items
 function removeItem (deleteElement) {
     deleteElement.parentElement.remove()
 }
@@ -26,8 +40,8 @@ function removeItem (deleteElement) {
 
 ul.addEventListener("click", function (e) {
   switch(e.target.className) {
-    case "li":
-      showEditInput();
+    case "listItem":
+      lineThrough(e.target);  //need to create function for line through
       break;
     case "delete":
       removeItem(e.target);
@@ -48,3 +62,17 @@ input.addEventListener("keypress", function (event) {
     createListItem();
   }
 });
+
+// save user's to do list in local storage
+
+let savedToDo = localStorage.getItem("to-do-list");
+// get items from local storage
+if (savedToDo !== null) {
+  let parsedToDo = JSON.parse(savedToDo);
+  listArr = parsedToDo;
+  for (let toDo of parsedToDo) {
+    console.log(toDo);
+  }
+}
+
+// show items on to do list when it is refreshed
